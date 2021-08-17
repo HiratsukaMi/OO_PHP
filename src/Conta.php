a30c92b5253ff973276ff518495b479c9f74d158
Michelly Hiratsuka Zorzi <michelly.hiratsuka@gmail.com>
2021-08-17 18:11:02 -0300

    Exercícios cap 3 - Métodos

--- src/Conta.php
+++ src/Conta.php
@@ -6,6 +6,40 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar): void
+    {
+        //$this - referencia o objeto que utilizou o método.
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return; //essa linha finaliza o método quando ele for 'verdadeiro', deixando de executar as operações abaixo dela.
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    /*no método transferir foram definidas duas variáveis: valorATransferir e Conta $contaDestino.
+      A segunda variável serve como referência para, no caso, onde o dinheiro sacado irá.*/
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
 
 /*
